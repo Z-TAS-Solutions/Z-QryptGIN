86716a0300a34d68978c5bc6d8940ade3d342b77
Cyberlowspecs <oshamanbhany@gmail.com>
2026-03-20 01:41:29 +0530

    1) updated the module name for global uniqueness 2) made the config.go file as well as the constants to which are required for database connection and Persist Login

---
+++ configs/config.go
@@ -0,0 +1,69 @@
+package configs
+
+import (
+	"fmt"
+	"internal/app/constants"
+	"net/http"
+	"os"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	Server   serverConfig
+	Database databaseConfig
+}
+
+type serverConfig struct {
+	Address string
+}
+
+type databaseConfig struct {
+	DatabaseDriver string
+	DatabaseSource string
+}
+
+func NewConfig() *Config {
+	err := godotenv.Load("configs/dev.env")
+	if err != nil {
+		panic("Error loading .env file")
+	}
+
+	c := &Config{
+		Server: serverConfig{
+			Address: GetEnvOrPanic(constants.EnvKeys.ServerAddress),
+		},
+		Database: databaseConfig{
+			DatabaseDriver: GetEnvOrPanic(constants.EnvKeys.DBDriver),
+			DatabaseSource: GetEnvOrPanic(constants.EnvKeys.DBSource),
+		},
+	}
+
+	return c
+}
+
+func GetEnvOrPanic(key string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		panic(fmt.Sprintf("environment variable %s not set", key))
+	}
+
+	return value
+}
+
+func (conf *Config) CorsNew() gin.HandlerFunc {
+	allowedOrigin := GetEnvOrPanic(constants.EnvKeys.CorsAllowedOrigin)
+
+	return cors.New(cors.Config{
+		AllowMethods:     []string{http.MethodGet, http.MethodPost},
+		AllowHeaders:     []string{constants.Headers.Origin},
+		ExposeHeaders:    []string{constants.Headers.ContentLength},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == allowedOrigin
+		},
+		MaxAge: constants.MaxAge,
+	})
+}
