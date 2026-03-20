ad0df7996e317f09467ccc936309c923a94f3ad1
Cyberlowspecs <oshamanbhany@gmail.com>
2026-03-20 17:34:05 +0530

    Implemented the middlewear required to parse the GORM validation errors to JSON and send them over via HTTP response

---
+++ api/server/middlewear.go
@@ -0,0 +1,62 @@
+package server
+
+import (
+	"errors"
+	"net/http"
+	"github.com/Z-TAS-Solutions/Z-QryptGIN/internal/app/database" // Update with your actual module name
+	"github.com/gin-gonic/gin"
+)
+
+func ErrorHandler() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Next()
+
+		if len(c.Errors) > 0 {
+			err := c.Errors.Last().Err
+
+			if isValidationError(err) {
+				c.JSON(http.StatusBadRequest, gin.H{
+					"status":  "error",
+					"code":    400,
+					"message": err.Error(),
+				})
+				return
+			}
+
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"status":  "error",
+				"code":    500,
+				"message": "an internal server error occurred",
+			})
+		}
+	}
+}
+
+func isValidationError(err error) bool {
+	validationErrors := []error{
+		database.ErrInvalidEmail,
+		database.ErrInvalidPhone,
+		database.ErrInvalidIPv4,
+		database.ErrInvalidNIC,
+		database.ErrInvalidUserID,
+		database.ErrInvalidNotificationID,
+		database.ErrInvalidMfaID,
+		database.ErrInvalidActivityID,
+		database.ErrInvalidPasskeyID,
+		database.ErrInvalidSessionID,
+		database.ErrInvalidStatus,
+		database.ErrInvalidSecurityLevel,
+		database.ErrInvalidNotifyType,
+		database.ErrInvalidActivityType,
+		database.ErrInvalidMfaStatus,
+		database.ErrInvalidMfaDecision,
+		database.ErrInvalidRole,
+	}
+
+	for _, vErr := range validationErrors {
+		if errors.Is(err, vErr) {
+			return true
+		}
+	}
+	return false
+}
