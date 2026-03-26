7be40b1a3f4ce4646cfcd9ba6a3ec7f77eb16176
zischl <supermechs456@gmail.com>
2026-03-26 12:37:03 +0530

    - Added unit tests for ToF lifecycle and LED status - Verified remote LED state changes - Added tests for ToF enable/disable/activate/deactivate flows - Covered threshold configuration (150mm, 200mm) including reconfigure while active - Asserted success messages for enable/activate operations - All tests successful.

---
+++ api/grpc/zpi_client/zpi_client_tests.go
@@ -0,0 +1,65 @@
+package zpi_client
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/Z-TAS-Solutions/Z-QryptGIN/internal/pkg/zscanproto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func RunZPiClientTest(ip string) zscanproto.ZPiControllerClient {
+	conn, err := grpc.Dial(ip, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("failed to connect: %v", err)
+	}
+
+	//ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	//defer cancel()
+
+	client := zscanproto.NewZPiControllerClient(conn)
+	log.Print("Connected to ZPi GRPC Host!")
+
+	statusResp, _ := client.SetLEDStatus(context.Background(), &zscanproto.LEDStatusRequest{
+		Status: zscanproto.LEDStatus_SUCCESS,
+	})
+	fmt.Println("SetLEDStatus:", statusResp.Message)
+
+	ledState, _ := client.GetLED(context.Background(), &zscanproto.Empty{})
+	fmt.Printf("LED State - R:%d G:%d B:%d\n", ledState.Red, ledState.Green, ledState.Blue)
+
+	tofConfigResp, _ := client.ConfigureToF(context.Background(), &zscanproto.ToFConfig{
+		Threshold: 150,
+	})
+	fmt.Println("ConfigureToF:", tofConfigResp.Message)
+
+	enableResp, _ := client.EnableToF(context.Background(), &zscanproto.Empty{})
+	fmt.Println("EnableToF:", enableResp.Message)
+
+	activateResp, _ := client.ActivateToF(context.Background(), &zscanproto.Empty{})
+	fmt.Println("ActivateToF:", activateResp.Message)
+
+	time.Sleep(500 * time.Millisecond)
+
+	deactivateResp, _ := client.DeactivateToF(context.Background(), &zscanproto.Empty{})
+	fmt.Println("DeactivateToF:", deactivateResp.Message)
+
+	disableResp, _ := client.DisableToF(context.Background(), &zscanproto.Empty{})
+	fmt.Println("DisableToF:", disableResp.Message)
+
+	response, _ := client.EnableToF(context.Background(), &zscanproto.Empty{})
+	fmt.Println(response)
+
+	response, _ = client.ActivateToF(context.Background(), &zscanproto.Empty{})
+	fmt.Println(response)
+
+	reconfigResp, _ := client.ConfigureToF(context.Background(), &zscanproto.ToFConfig{
+		Threshold: 200,
+	})
+	fmt.Println("ReconfigureToF while active:", reconfigResp.Message)
+
+	return client
+}
--- cmd/grpc/main.go
+++ cmd/grpc/main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"log"
 	"net"
 
 	"github.com/Z-TAS-Solutions/Z-QryptGIN/api/grpc/zpi_client"
 	"github.com/Z-TAS-Solutions/Z-QryptGIN/internal/pkg/ipc"
-	"github.com/Z-TAS-Solutions/Z-QryptGIN/internal/pkg/zscanproto"
 	"google.golang.org/grpc"
 )
 
@@ -49,19 +46,6 @@ func RunRemoteGRPC(compute bool, remoteAddr string) {
 
 func main() {
 	//RunLocalGRPC(true)
-	client := zpi_client.RunZPiClient("192.168.1.229:50051")
-
-	//ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
-	//defer cancel()
-
-	statusResp, err := client.SetLEDStatus(context.Background(), &zscanproto.LEDStatusRequest{
-		Status: zscanproto.LEDStatus_PENDING,
-	})
-
-	if err != nil {
-		log.Fatalf("RPC failed: %v", err)
-	}
-
-	fmt.Println("SetLEDStatus:", statusResp.Message)
+	zpi_client.RunZPiClientTest("192.168.1.229:50051")
 
 }
