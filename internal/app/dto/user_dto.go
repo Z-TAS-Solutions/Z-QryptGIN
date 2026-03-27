38e7aabffdc7a12cf5263a5498cafbd77ce4affa
Cyberlowspecs <oshamanbhany@gmail.com>
2026-03-27 13:41:31 +0530

    updated the CreateUserRequest by including the proper types from the database package

--- internal/app/dto/user_dto.go
+++ internal/app/dto/user_dto.go
@@ -1,12 +1,16 @@
 package dto
 
+import (
+	"github.com/Z-TAS-Solutions/Z-QryptGIN/internal/app/database"
+)
+
 // CreateUserRequest is what the client sends
 type CreateUserRequest struct {
-	Name     string      `json:"name" binding:"required"`
-	Email    Email       `json:"email" binding:"required,email"`
-	PhoneNo  PhoneNumber `json:"phone_no" binding:"required"`
-	Nic      NIC         `json:"nic" binding:"required"`
-	Password string      `json:"password" binding:"required,min=8"`
+	Name     string               `json:"name" binding:"required"`
+	Email    database.Email       `json:"email" binding:"required,email"`
+	PhoneNo  database.PhoneNumber `json:"phone_no" binding:"required"`
+	Nic      database.NIC         `json:"nic" binding:"required"`
+	Password string               `json:"password" binding:"required,min=8"`
 }
 
 // UserResponse is what the client receives (no passwords!)
